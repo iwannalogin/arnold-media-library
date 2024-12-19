194f862fd60b04c411971ddc192118dfac24aa6f
Matthew Barthel <mdevonb@hotmail.com>
2024-12-19 08:14:06 -0500

    Updated FileLibrary

--- Models/FileLibrary.cs
+++ Models/FileLibrary.cs
@@ -1,10 +1,12 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
 namespace arnold.Models;
 
 public class FileLibrary {
     [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public long Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
